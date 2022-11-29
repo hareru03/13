a2093b032d9069ff1a35792629ec659da41e6856
hareru03 <hareru03@sookmyung.ac.kr>
2022-11-29 17:37:44 +0900

    01

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student {
+	
+	int ID;
+	char name[10];
+	double grade;
+};
+
+int main(int argc, char *argv[]) {
+	
+	struct student s = {123, "kIM", 3.9};
+	s.ID = 123456;
+	strcpy(s.name,"Juyeop");
+	s.grade = 4.2;
+	
+	printf("ID : %d\n",s.ID);
+	printf("name : %s\n",s.name);
+	printf("grade : %f\n", s.grade);
+	
+	return 0;
+}
